1c7af25aef65fa81daf07a51168cff3f20777084
Andrey Mork <35698586+AndreyMork@users.noreply.github.com>
2021-03-27 18:55:32 +0300

    refactor: style changes

--- src/ashigaru_health/main.clj
+++ src/ashigaru_health/main.clj
@@ -1,7 +1,7 @@
 (ns ashigaru-health.main
   (:gen-class)
   (:require [ashigaru-health.app :refer [get-app]]
-            [ashigaru-health.config :refer [load-and-validate-config]]
+            [ashigaru-health.config :as config]
             [ashigaru-health.db.core :as db]
             [orchestra.spec.test]
             [ring.adapter.jetty :refer [run-jetty]]))
@@ -10,7 +10,7 @@
   [& _args]
   (let [{app-config :app
          server-config :server
-         db-config :db} (load-and-validate-config)
+         db-config :db} (config/load-and-validate-config)
         db-datasource (db/get-connection-pool db-config)
         app (get-app {:logging? (:logging app-config)
                       :connection-pool db-datasource})]
